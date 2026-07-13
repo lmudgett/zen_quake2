6b9e3d1a12cc8a71d313544174551866e0ba2a88
Len Mudgett <lmudgett@gmail.com>
2026-07-13 03:02:58 -0400

    Phase 0: 64-bit dedicated server builds with SDL3 platform layer
    - CMake build (VS2022 x64): q2ded + baseq2/ctf game modules, SDL3 via
      FetchContent from libsdl-org/SDL main
    - New SDL3 platform layer: sdl/sys_sdl.c (timing, dynlib, console, hunk,
      dir scan), sdl/net_sockets.c (BSD sockets + WinSock2, IPX dropped)
    - Portable core copied from the reference tree into src/ (qcommon,
      server, game, ctf, null stubs)
    - 64-bit fixes: qboolean vs stdbool macro collision, x86_64/arm64
      CPUSTRING, offsetof-based FOFS/STOFS/LLOFS/CLOFS and STRUCT_FROM_LINK
      (kills all 230 pointer-truncation warnings)

---
+++ src/sdl/net_sockets.c
@@ -0,0 +1,517 @@
+// net_sockets.c -- portable UDP networking: BSD sockets + WinSock2.
+// Replaces win32/net_wins.c and linux/net_udp.c. IPX is gone for good.
+
+#include "../qcommon/qcommon.h"
+
+#ifdef _WIN32
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+typedef SOCKET	qsocket_t;
+#define Q_INVALID_SOCKET	INVALID_SOCKET
+#define Q_SOCKET_ERROR		SOCKET_ERROR
+#define Q_CLOSESOCKET		closesocket
+#define Q_SOCKERRNO()		WSAGetLastError()
+#define Q_EWOULDBLOCK		WSAEWOULDBLOCK
+#define Q_ECONNREFUSED		WSAECONNREFUSED
+
+#else
+
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/param.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+
+typedef int		qsocket_t;
+#define Q_INVALID_SOCKET	(-1)
+#define Q_SOCKET_ERROR		(-1)
+#define Q_CLOSESOCKET		close
+#define Q_SOCKERRNO()		errno
+#define Q_EWOULDBLOCK		EWOULDBLOCK
+#define Q_ECONNREFUSED		ECONNREFUSED
+
+#endif
+
+netadr_t	net_local_adr;
+
+#define	MAX_LOOPBACK	4
+
+typedef struct
+{
+	byte	data[MAX_MSGLEN];
+	int		datalen;
+} loopmsg_t;
+
+typedef struct
+{
+	loopmsg_t	msgs[MAX_LOOPBACK];
+	int			get, send;
+} loopback_t;
+
+static loopback_t	loopbacks[2];
+static qsocket_t	ip_sockets[2] = { Q_INVALID_SOCKET, Q_INVALID_SOCKET };
+
+static qsocket_t NET_Socket (char *net_interface, int port);
+static char *NET_ErrorString (void);
+
+//=============================================================================
+
+static void NetadrToSockadr (netadr_t *a, struct sockaddr_in *s)
+{
+	memset (s, 0, sizeof(*s));
+
+	if (a->type == NA_BROADCAST)
+	{
+		s->sin_family = AF_INET;
+		s->sin_port = a->port;
+		s->sin_addr.s_addr = INADDR_BROADCAST;
+	}
+	else if (a->type == NA_IP)
+	{
+		s->sin_family = AF_INET;
+		memcpy (&s->sin_addr, a->ip, 4);
+		s->sin_port = a->port;
+	}
+}
+
+static void SockadrToNetadr (struct sockaddr_in *s, netadr_t *a)
+{
+	memcpy (a->ip, &s->sin_addr, 4);
+	a->port = s->sin_port;
+	a->type = NA_IP;
+}
+
+qboolean NET_CompareAdr (netadr_t a, netadr_t b)
+{
+	if (a.ip[0] == b.ip[0] && a.ip[1] == b.ip[1] && a.ip[2] == b.ip[2] && a.ip[3] == b.ip[3] && a.port == b.port)
+		return true;
+	return false;
+}
+
+/*
+===================
+NET_CompareBaseAdr
+
+Compares without the port
+===================
+*/
+qboolean NET_CompareBaseAdr (netadr_t a, netadr_t b)
+{
+	if (a.type != b.type)
+		return false;
+
+	if (a.type == NA_LOOPBACK)
+		return true;
+
+	if (a.type == NA_IP)
+	{
+		if (a.ip[0] == b.ip[0] && a.ip[1] == b.ip[1] && a.ip[2] == b.ip[2] && a.ip[3] == b.ip[3])
+			return true;
+		return false;
+	}
+
+	return false;
+}
+
+char *NET_AdrToString (netadr_t a)
+{
+	static	char	s[64];
+
+	Com_sprintf (s, sizeof(s), "%i.%i.%i.%i:%i", a.ip[0], a.ip[1], a.ip[2], a.ip[3], ntohs(a.port));
+
+	return s;
+}
+
+char *NET_BaseAdrToString (netadr_t a)
+{
+	static	char	s[64];
+
+	Com_sprintf (s, sizeof(s), "%i.%i.%i.%i", a.ip[0], a.ip[1], a.ip[2], a.ip[3]);
+
+	return s;
+}
+
+/*
+=============
+NET_StringToSockaddr
+
+localhost
+idnewt
+idnewt:28000
+192.246.40.70
+192.246.40.70:28000
+=============
+*/
+static qboolean NET_StringToSockaddr (char *s, struct sockaddr_in *sadr)
+{
+	struct addrinfo	hints, *res;
+	char	*colon;
+	char	copy[128];
+
+	memset (sadr, 0, sizeof(*sadr));
+	sadr->sin_family = AF_INET;
+	sadr->sin_port = 0;
+
+	strncpy (copy, s, sizeof(copy)-1);
+	copy[sizeof(copy)-1] = 0;
+
+	// strip off a trailing :port if present
+	for (colon = copy ; *colon ; colon++)
+		if (*colon == ':')
+		{
+			*colon = 0;
+			sadr->sin_port = htons((short)atoi(colon+1));
+		}
+
+	memset (&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+
+	if (getaddrinfo (copy, NULL, &hints, &res) != 0 || !res)
+		return false;
+
+	sadr->sin_addr = ((struct sockaddr_in *)res->ai_addr)->sin_addr;
+	freeaddrinfo (res);
+
+	return true;
+}
+
+qboolean NET_StringToAdr (char *s, netadr_t *a)
+{
+	struct sockaddr_in sadr;
+
+	if (!strcmp (s, "localhost"))
+	{
+		memset (a, 0, sizeof(*a));
+		a->type = NA_LOOPBACK;
+		return true;
+	}
+
+	if (!NET_StringToSockaddr (s, &sadr))
+		return false;
+
+	SockadrToNetadr (&sadr, a);
+
+	return true;
+}
+
+qboolean NET_IsLocalAddress (netadr_t adr)
+{
+	return NET_CompareAdr (adr, net_local_adr);
+}
+
+/*
+=============================================================================
+
+LOOPBACK BUFFERS FOR LOCAL PLAYER
+
+=============================================================================
+*/
+
+static qboolean NET_GetLoopPacket (netsrc_t sock, netadr_t *net_from, sizebuf_t *net_message)
+{
+	int			i;
+	loopback_t	*loop;
+
+	loop = &loopbacks[sock];
+
+	if (loop->send - loop->get > MAX_LOOPBACK)
+		loop->get = loop->send - MAX_LOOPBACK;
+
+	if (loop->get >= loop->send)
+		return false;
+
+	i = loop->get & (MAX_LOOPBACK-1);
+	loop->get++;
+
+	memcpy (net_message->data, loop->msgs[i].data, loop->msgs[i].datalen);
+	net_message->cursize = loop->msgs[i].datalen;
+	*net_from = net_local_adr;
+	return true;
+}
+
+static void NET_SendLoopPacket (netsrc_t sock, int length, void *data, netadr_t to)
+{
+	int			i;
+	loopback_t	*loop;
+
+	loop = &loopbacks[sock^1];
+
+	i = loop->send & (MAX_LOOPBACK-1);
+	loop->send++;
+
+	memcpy (loop->msgs[i].data, data, length);
+	loop->msgs[i].datalen = length;
+}
+
+//=============================================================================
+
+qboolean NET_GetPacket (netsrc_t sock, netadr_t *net_from, sizebuf_t *net_message)
+{
+	int		ret;
+	struct sockaddr_in	from;
+	socklen_t	fromlen;
+	qsocket_t	net_socket;
+	int		err;
+
+	if (NET_GetLoopPacket (sock, net_from, net_message))
+		return true;
+
+	net_socket = ip_sockets[sock];
+	if (net_socket == Q_INVALID_SOCKET)
+		return false;
+
+	fromlen = sizeof(from);
+	ret = recvfrom (net_socket, (char *)net_message->data, net_message->maxsize,
+		0, (struct sockaddr *)&from, &fromlen);
+	if (ret == Q_SOCKET_ERROR)
+	{
+		err = Q_SOCKERRNO ();
+
+		if (err == Q_EWOULDBLOCK || err == Q_ECONNREFUSED)
+			return false;
+		Com_Printf ("NET_GetPacket: %s\n", NET_ErrorString());
+		return false;
+	}
+
+	SockadrToNetadr (&from, net_from);
+
+	if (ret == net_message->maxsize)
+	{
+		Com_Printf ("Oversize packet from %s\n", NET_AdrToString (*net_from));
+		return false;
+	}
+
+	net_message->cursize = ret;
+	return true;
+}
+
+//=============================================================================
+
+void NET_SendPacket (netsrc_t sock, int length, void *data, netadr_t to)
+{
+	int		ret;
+	struct sockaddr_in	addr;
+	qsocket_t	net_socket;
+
+	if ( to.type == NA_LOOPBACK )
+	{
+		NET_SendLoopPacket (sock, length, data, to);
+		return;
+	}
+
+	if (to.type == NA_BROADCAST || to.type == NA_IP)
+	{
+		net_socket = ip_sockets[sock];
+		if (net_socket == Q_INVALID_SOCKET)
+			return;
+	}
+	else
+	{
+		Com_Error (ERR_FATAL, "NET_SendPacket: bad address type");
+		return;
+	}
+
+	NetadrToSockadr (&to, &addr);
+
+	ret = sendto (net_socket, data, length, 0, (struct sockaddr *)&addr, sizeof(addr) );
+	if (ret == Q_SOCKET_ERROR)
+	{
+		Com_Printf ("NET_SendPacket ERROR: %s\n", NET_ErrorString());
+	}
+}
+
+//=============================================================================
+
+/*
+====================
+NET_OpenIP
+====================
+*/
+static void NET_OpenIP (void)
+{
+	cvar_t	*port, *ip;
+
+	port = Cvar_Get ("port", va("%i", PORT_SERVER), CVAR_NOSET);
+	ip = Cvar_Get ("ip", "localhost", CVAR_NOSET);
+
+	if (ip_sockets[NS_SERVER] == Q_INVALID_SOCKET)
+		ip_sockets[NS_SERVER] = NET_Socket (ip->string, port->value);
+	if (ip_sockets[NS_CLIENT] == Q_INVALID_SOCKET)
+		ip_sockets[NS_CLIENT] = NET_Socket (ip->string, PORT_ANY);
+}
+
+/*
+====================
+NET_Config
+
+A single player game will only use the loopback code
+====================
+*/
+void NET_Config (qboolean multiplayer)
+{
+	int		i;
+
+	if (!multiplayer)
+	{	// shut down any existing sockets
+		for (i=0 ; i<2 ; i++)
+		{
+			if (ip_sockets[i] != Q_INVALID_SOCKET)
+			{
+				Q_CLOSESOCKET (ip_sockets[i]);
+				ip_sockets[i] = Q_INVALID_SOCKET;
+			}
+		}
+	}
+	else
+	{	// open sockets
+		NET_OpenIP ();
+	}
+}
+
+//===================================================================
+
+/*
+====================
+NET_Init
+====================
+*/
+void NET_Init (void)
+{
+#ifdef _WIN32
+	WSADATA	winsockdata;
+
+	if (WSAStartup (MAKEWORD(2,2), &winsockdata))
+		Com_Error (ERR_FATAL, "Winsock initialization failed.");
+
+	Com_Printf ("Winsock Initialized\n");
+#endif
+}
+
+/*
+====================
+NET_Socket
+====================
+*/
+static qsocket_t NET_Socket (char *net_interface, int port)
+{
+	qsocket_t	newsocket;
+	struct sockaddr_in	address;
+#ifdef _WIN32
+	u_long	_true = 1;
+#else
+	int		_true = 1;
+#endif
+	int		i = 1;
+
+	if ((newsocket = socket (PF_INET, SOCK_DGRAM, IPPROTO_UDP)) == Q_INVALID_SOCKET)
+	{
+		Com_Printf ("ERROR: UDP_OpenSocket: socket: %s\n", NET_ErrorString());
+		return Q_INVALID_SOCKET;
+	}
+
+	// make it non-blocking
+#ifdef _WIN32
+	if (ioctlsocket (newsocket, FIONBIO, &_true) == Q_SOCKET_ERROR)
+#else
+	if (ioctl (newsocket, FIONBIO, &_true) == Q_SOCKET_ERROR)
+#endif
+	{
+		Com_Printf ("ERROR: UDP_OpenSocket: ioctl FIONBIO: %s\n", NET_ErrorString());
+		Q_CLOSESOCKET (newsocket);
+		return Q_INVALID_SOCKET;
+	}
+
+	// make it broadcast capable
+	if (setsockopt(newsocket, SOL_SOCKET, SO_BROADCAST, (char *)&i, sizeof(i)) == Q_SOCKET_ERROR)
+	{
+		Com_Printf ("ERROR: UDP_OpenSocket: setsockopt SO_BROADCAST: %s\n", NET_ErrorString());
+		Q_CLOSESOCKET (newsocket);
+		return Q_INVALID_SOCKET;
+	}
+
+	memset (&address, 0, sizeof(address));
+
+	if (!net_interface || !net_interface[0] || !Q_stricmp(net_interface, "localhost"))
+		address.sin_addr.s_addr = INADDR_ANY;
+	else
+		NET_StringToSockaddr (net_interface, &address);
+
+	if (port == PORT_ANY)
+		address.sin_port = 0;
+	else
+		address.sin_port = htons((short)port);
+
+	address.sin_family = AF_INET;
+
+	if (bind (newsocket, (struct sockaddr *)&address, sizeof(address)) == Q_SOCKET_ERROR)
+	{
+		Com_Printf ("ERROR: UDP_OpenSocket: bind: %s\n", NET_ErrorString());
+		Q_CLOSESOCKET (newsocket);
+		return Q_INVALID_SOCKET;
+	}
+
+	return newsocket;
+}
+
+/*
+====================
+NET_Shutdown
+====================
+*/
+void NET_Shutdown (void)
+{
+	NET_Config (false);	// close sockets
+
+#ifdef _WIN32
+	WSACleanup ();
+#endif
+}
+
+/*
+====================
+NET_ErrorString
+====================
+*/
+static char *NET_ErrorString (void)
+{
+#ifdef _WIN32
+	static char	msg[256];
+	int		code;
+
+	code = WSAGetLastError ();
+	if (!FormatMessageA (FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+			NULL, code, 0, msg, sizeof(msg), NULL))
+		Com_sprintf (msg, sizeof(msg), "winsock error %d", code);
+	return msg;
+#else
+	return strerror (errno);
+#endif
+}
+
+// sleeps msec or until net socket is ready
+void NET_Sleep (int msec)
+{
+	struct timeval	timeout;
+	fd_set	fdset;
+	extern qboolean stdin_active;
+
+	if (ip_sockets[NS_SERVER] == Q_INVALID_SOCKET || (dedicated && !dedicated->value))
+		return; // we're not a server, just run full speed
+
+	FD_ZERO (&fdset);
+#ifndef _WIN32
+	if (stdin_active)
+		FD_SET (0, &fdset); // stdin is processed too
+#endif
+	FD_SET (ip_sockets[NS_SERVER], &fdset); // network socket
+	timeout.tv_sec = msec/1000;
+	timeout.tv_usec = (msec%1000)*1000;
+	select ((int)(ip_sockets[NS_SERVER]+1), &fdset, NULL, NULL, &timeout);
+}
---
+++ src/sdl/sys_sdl.c
@@ -0,0 +1,614 @@
+// sys_sdl.c -- SDL3 system services: timing, dynamic libraries, console,
+// hunk allocation, directory scanning. Replaces win32/sys_win.c +
+// win32/q_shwin.c and linux/sys_linux.c + linux/q_shlinux.c.
+
+#include <SDL3/SDL.h>
+
+#include "../qcommon/qcommon.h"
+
+#include <errno.h>
+#include <stdio.h>
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <direct.h>
+#include <io.h>
+#include <conio.h>
+#else
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <fnmatch.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#endif
+
+int			curtime;
+unsigned	sys_frame_time;
+
+qboolean	stdin_active = true;
+
+/*
+===============================================================================
+
+SYSTEM IO
+
+===============================================================================
+*/
+
+void Sys_Error (char *error, ...)
+{
+	va_list		argptr;
+	char		text[1024];
+
+	CL_Shutdown ();
+	Qcommon_Shutdown ();
+
+	va_start (argptr, error);
+	vsnprintf (text, sizeof(text), error, argptr);
+	va_end (argptr);
+
+	fprintf (stderr, "Sys_Error: %s\n", text);
+
+	if (!dedicated || !dedicated->value)
+		SDL_ShowSimpleMessageBox (SDL_MESSAGEBOX_ERROR, "Quake 2 Error", text, NULL);
+
+	exit (1);
+}
+
+void Sys_Quit (void)
+{
+	CL_Shutdown ();
+	Qcommon_Shutdown ();
+	exit (0);
+}
+
+void Sys_Init (void)
+{
+}
+
+void Sys_AppActivate (void)
+{
+}
+
+void Sys_CopyProtect (void)
+{
+}
+
+/*
+================
+Sys_ConsoleInput
+
+Non-blocking line input from the dedicated server terminal
+================
+*/
+static char	console_text[256];
+static int	console_textlen;
+
+char *Sys_ConsoleInput (void)
+{
+	if (!dedicated || !dedicated->value)
+		return NULL;
+
+#ifdef _WIN32
+	while (_kbhit ())
+	{
+		int ch = _getch ();
+
+		switch (ch)
+		{
+		case '\r':
+		case '\n':
+			putchar ('\n');
+			if (console_textlen)
+			{
+				console_text[console_textlen] = 0;
+				console_textlen = 0;
+				return console_text;
+			}
+			break;
+
+		case '\b':
+		case 127:
+			if (console_textlen)
+			{
+				console_textlen--;
+				fputs ("\b \b", stdout);
+				fflush (stdout);
+			}
+			break;
+
+		default:
+			if (ch >= ' ' && console_textlen < (int)sizeof(console_text)-2)
+			{
+				putchar (ch);
+				fflush (stdout);
+				console_text[console_textlen++] = ch;
+			}
+			break;
+		}
+	}
+	return NULL;
+#else
+	fd_set			fdset;
+	struct timeval	timeout;
+	int				len;
+
+	if (!stdin_active)
+		return NULL;
+
+	FD_ZERO (&fdset);
+	FD_SET (0, &fdset);	// stdin
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 0;
+	if (select (1, &fdset, NULL, NULL, &timeout) == -1 || !FD_ISSET(0, &fdset))
+		return NULL;
+
+	len = read (0, console_text, sizeof(console_text));
+	if (len == 0)	// eof
+	{
+		stdin_active = false;
+		return NULL;
+	}
+	if (len < 1)
+		return NULL;
+	console_text[len-1] = 0;	// rip off the \n and terminate
+
+	return console_text;
+#endif
+}
+
+/*
+================
+Sys_ConsoleOutput
+
+Print text to the dedicated console
+================
+*/
+void Sys_ConsoleOutput (char *string)
+{
+	if (!dedicated || !dedicated->value)
+		return;
+
+	fputs (string, stdout);
+	fflush (stdout);
+}
+
+/*
+================
+Sys_SendKeyEvents
+
+The SDL event pump lives in the client input layer (in_sdl.c);
+for the dedicated server there are no events to send.
+================
+*/
+void Sys_SendKeyEvents (void)
+{
+#ifndef DEDICATED_ONLY
+	void IN_Update (void);
+	IN_Update ();
+#endif
+
+	// grab frame time
+	sys_frame_time = Sys_Milliseconds ();
+}
+
+char *Sys_GetClipboardData (void)
+{
+	char	*data = NULL;
+	char	*cliptext;
+
+	if (!SDL_WasInit (SDL_INIT_VIDEO))
+		return NULL;
+
+	cliptext = SDL_GetClipboardText ();
+	if (cliptext && cliptext[0])
+	{
+		data = malloc (strlen(cliptext) + 1);
+		strcpy (data, cliptext);
+	}
+	SDL_free (cliptext);
+
+	return data;
+}
+
+/*
+========================================================================
+
+GAME DLL
+
+========================================================================
+*/
+
+static SDL_SharedObject	*game_library;
+
+#ifdef _WIN32
+static const char *gamename = "game_x86_64.dll";
+#elif defined(__APPLE__)
+static const char *gamename = "game_x86_64.dylib";
+#else
+static const char *gamename = "game_x86_64.so";
+#endif
+
+void Sys_UnloadGame (void)
+{
+	if (game_library)
+		SDL_UnloadObject (game_library);
+	game_library = NULL;
+}
+
+/*
+=================
+Sys_GetGameAPI
+
+Loads the game dll
+=================
+*/
+void *Sys_GetGameAPI (void *parms)
+{
+	void	*(*GetGameAPI) (void *);
+	char	name[MAX_OSPATH];
+	char	*path;
+
+	if (game_library)
+		Com_Error (ERR_FATAL, "Sys_GetGameAPI without Sys_UnloadGame");
+
+	// run through the filesystem search paths
+	path = NULL;
+	while (1)
+	{
+		path = FS_NextPath (path);
+		if (!path)
+			return NULL;		// couldn't find one anywhere
+		Com_sprintf (name, sizeof(name), "%s/%s", path, gamename);
+		game_library = SDL_LoadObject (name);
+		if (game_library)
+		{
+			Com_DPrintf ("SDL_LoadObject (%s)\n", name);
+			break;
+		}
+	}
+
+	GetGameAPI = (void *(*)(void *))SDL_LoadFunction (game_library, "GetGameAPI");
+	if (!GetGameAPI)
+	{
+		Sys_UnloadGame ();
+		return NULL;
+	}
+
+	return GetGameAPI (parms);
+}
+
+/*
+========================================================================
+
+HUNK ALLOCATION
+
+Reserve a large address range, commit/trim as the level data loads.
+
+========================================================================
+*/
+
+static byte	*membase;
+static int	maxhunksize;
+static int	curhunksize;
+
+#ifdef _WIN32
+
+void *Hunk_Begin (int maxsize)
+{
+	curhunksize = 0;
+	maxhunksize = maxsize;
+	membase = VirtualAlloc (NULL, maxhunksize, MEM_RESERVE, PAGE_NOACCESS);
+	if (!membase)
+		Sys_Error ("Hunk_Begin: VirtualAlloc reserve failed");
+	return (void *)membase;
+}
+
+void *Hunk_Alloc (int size)
+{
+	void	*buf;
+
+	// round to cacheline
+	size = (size+31)&~31;
+
+	// commit pages as needed
+	buf = VirtualAlloc (membase, curhunksize+size, MEM_COMMIT, PAGE_READWRITE);
+	if (!buf)
+		Sys_Error ("Hunk_Alloc: VirtualAlloc commit failed");
+	curhunksize += size;
+	if (curhunksize > maxhunksize)
+		Sys_Error ("Hunk_Alloc overflow");
+
+	return (void *)(membase+curhunksize-size);
+}
+
+int Hunk_End (void)
+{
+	return curhunksize;
+}
+
+void Hunk_Free (void *base)
+{
+	if (base)
+		VirtualFree (base, 0, MEM_RELEASE);
+}
+
+#else	// POSIX: mmap the full range, trim the unused tail in Hunk_End
+
+// a size header is stored at the start of the mapping so Hunk_Free
+// knows how much to munmap after Hunk_End trimmed the region
+#define HUNK_HEADER	32		// keep Hunk_Alloc's cacheline alignment
+
+void *Hunk_Begin (int maxsize)
+{
+	maxhunksize = maxsize + HUNK_HEADER;
+	curhunksize = 0;
+	membase = mmap (0, maxhunksize, PROT_READ|PROT_WRITE,
+		MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+	if (membase == NULL || membase == (byte *)-1)
+		Sys_Error ("Hunk_Begin: unable to mmap %d bytes", maxsize);
+
+	*((int *)membase) = curhunksize;
+
+	return membase + HUNK_HEADER;
+}
+
+void *Hunk_Alloc (int size)
+{
+	byte	*buf;
+
+	// round to cacheline
+	size = (size+31)&~31;
+	if (curhunksize + size > maxhunksize)
+		Sys_Error ("Hunk_Alloc overflow");
+	buf = membase + HUNK_HEADER + curhunksize;
+	curhunksize += size;
+	return buf;
+}
+
+int Hunk_End (void)
+{
+	long	pagesize;
+	int		used, trim;
+
+	pagesize = sysconf (_SC_PAGESIZE);
+	used = (curhunksize + HUNK_HEADER + pagesize-1) & ~(pagesize-1);
+	trim = maxhunksize - used;
+	if (trim > 0)
+		munmap (membase + used, trim);
+
+	*((int *)membase) = used;
+
+	return curhunksize;
+}
+
+void Hunk_Free (void *base)
+{
+	byte	*m;
+
+	if (base)
+	{
+		m = ((byte *)base) - HUNK_HEADER;
+		if (munmap (m, *((int *)m)))
+			Sys_Error ("Hunk_Free: munmap failed (%d)", errno);
+	}
+}
+
+#endif
+
+/*
+========================================================================
+
+TIMING AND FILE SYSTEM SCANNING
+
+========================================================================
+*/
+
+int Sys_Milliseconds (void)
+{
+	curtime = (int)SDL_GetTicks ();
+	return curtime;
+}
+
+void Sys_Mkdir (char *path)
+{
+#ifdef _WIN32
+	_mkdir (path);
+#else
+	mkdir (path, 0777);
+#endif
+}
+
+static char	findbase[MAX_OSPATH];
+static char	findpath[MAX_OSPATH];
+
+#ifdef _WIN32
+
+static intptr_t	findhandle = -1;
+
+static qboolean CompareAttributes (unsigned found, unsigned musthave, unsigned canthave)
+{
+	if ( ( found & _A_RDONLY ) && ( canthave & SFF_RDONLY ) )
+		return false;
+	if ( ( found & _A_HIDDEN ) && ( canthave & SFF_HIDDEN ) )
+		return false;
+	if ( ( found & _A_SYSTEM ) && ( canthave & SFF_SYSTEM ) )
+		return false;
+	if ( ( found & _A_SUBDIR ) && ( canthave & SFF_SUBDIR ) )
+		return false;
+	if ( ( found & _A_ARCH ) && ( canthave & SFF_ARCH ) )
+		return false;
+
+	if ( ( musthave & SFF_RDONLY ) && !( found & _A_RDONLY ) )
+		return false;
+	if ( ( musthave & SFF_HIDDEN ) && !( found & _A_HIDDEN ) )
+		return false;
+	if ( ( musthave & SFF_SYSTEM ) && !( found & _A_SYSTEM ) )
+		return false;
+	if ( ( musthave & SFF_SUBDIR ) && !( found & _A_SUBDIR ) )
+		return false;
+	if ( ( musthave & SFF_ARCH ) && !( found & _A_ARCH ) )
+		return false;
+
+	return true;
+}
+
+char *Sys_FindFirst (char *path, unsigned musthave, unsigned canthave)
+{
+	struct _finddata_t findinfo;
+
+	if (findhandle != -1)
+		Sys_Error ("Sys_FindFirst without close");
+
+	COM_FilePath (path, findbase);
+	findhandle = _findfirst (path, &findinfo);
+	if (findhandle == -1)
+		return NULL;
+	if ( !CompareAttributes( findinfo.attrib, musthave, canthave ) )
+		return Sys_FindNext (musthave, canthave);
+	Com_sprintf (findpath, sizeof(findpath), "%s/%s", findbase, findinfo.name);
+	return findpath;
+}
+
+char *Sys_FindNext (unsigned musthave, unsigned canthave)
+{
+	struct _finddata_t findinfo;
+
+	if (findhandle == -1)
+		return NULL;
+	while (_findnext (findhandle, &findinfo) != -1)
+	{
+		if ( CompareAttributes( findinfo.attrib, musthave, canthave ) )
+		{
+			Com_sprintf (findpath, sizeof(findpath), "%s/%s", findbase, findinfo.name);
+			return findpath;
+		}
+	}
+	return NULL;
+}
+
+void Sys_FindClose (void)
+{
+	if (findhandle != -1)
+		_findclose (findhandle);
+	findhandle = -1;
+}
+
+#else	// POSIX
+
+static char	findpattern[MAX_OSPATH];
+static DIR	*fdir;
+
+static qboolean CompareAttributes (char *path, char *name,
+	unsigned musthave, unsigned canthave)
+{
+	struct stat st;
+	char fn[MAX_OSPATH];
+
+	// . and .. never match
+	if (strcmp(name, ".") == 0 || strcmp(name, "..") == 0)
+		return false;
+
+	Com_sprintf (fn, sizeof(fn), "%s/%s", path, name);
+	if (stat(fn, &st) == -1)
+		return false;	// shouldn't happen
+
+	if ( S_ISDIR(st.st_mode) && ( canthave & SFF_SUBDIR ) )
+		return false;
+
+	if ( ( musthave & SFF_SUBDIR ) && !S_ISDIR(st.st_mode) )
+		return false;
+
+	return true;
+}
+
+char *Sys_FindFirst (char *path, unsigned musthave, unsigned canthave)
+{
+	char *p;
+
+	if (fdir)
+		Sys_Error ("Sys_FindFirst without close");
+
+	strncpy (findbase, path, sizeof(findbase)-1);
+	findbase[sizeof(findbase)-1] = 0;
+
+	if ((p = strrchr(findbase, '/')) != NULL)
+	{
+		*p = 0;
+		strncpy (findpattern, p + 1, sizeof(findpattern)-1);
+		findpattern[sizeof(findpattern)-1] = 0;
+	}
+	else
+		strcpy (findpattern, "*");
+
+	if (strcmp(findpattern, "*.*") == 0)
+		strcpy (findpattern, "*");
+
+	if ((fdir = opendir(findbase)) == NULL)
+		return NULL;
+
+	return Sys_FindNext (musthave, canthave);
+}
+
+char *Sys_FindNext (unsigned musthave, unsigned canthave)
+{
+	struct dirent *d;
+
+	if (fdir == NULL)
+		return NULL;
+	while ((d = readdir(fdir)) != NULL)
+	{
+		if (!*findpattern || fnmatch(findpattern, d->d_name, 0) == 0)
+		{
+			if (CompareAttributes(findbase, d->d_name, musthave, canthave))
+			{
+				Com_sprintf (findpath, sizeof(findpath), "%s/%s", findbase, d->d_name);
+				return findpath;
+			}
+		}
+	}
+	return NULL;
+}
+
+void Sys_FindClose (void)
+{
+	if (fdir != NULL)
+		closedir (fdir);
+	fdir = NULL;
+}
+
+#endif
+
+//=============================================================================
+
+int main (int argc, char **argv)
+{
+	int	time, oldtime, newtime;
+
+	Qcommon_Init (argc, argv);
+
+	oldtime = Sys_Milliseconds ();
+
+	while (1)
+	{
+		// don't burn a core spinning; the server ticks at 10 Hz anyway
+		if (dedicated && dedicated->value)
+			SDL_Delay (1);
+
+		do
+		{
+			newtime = Sys_Milliseconds ();
+			time = newtime - oldtime;
+		} while (time < 1);
+
+		Qcommon_Frame (time);
+
+		oldtime = newtime;
+	}
+
+	// never gets here
+	return 0;
+}
